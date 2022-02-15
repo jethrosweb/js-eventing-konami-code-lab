b3d37b93e3dca699cff7810b6a5e357f30cdace0
Jethro Dean <jethroldean@gmail.com>
2022-02-15 16:48:24 +1100

    Completed assignment

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let keys = {37:'left',38:'up',39:'right',40:'down',65:'a',66:'b'}
+let code = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a']
 
-function init() {
-  // Write your JavaScript code inside the init() function
+document.addEventListener('keydown', checkCode, false);
 
+let keyCount = 0;
+
+function checkCode(event) {
+  let keyPressed = keys[event.keyCode];
+
+  if (keyPressed === code[keyCount]) {
+    keyCount++;
+
+    if (keyCount === code.length) {
+      cheatCodeActivated();
+      resetKeyState();
+    }
+  } else {
+    resetKeyState();
+  }
+}
+
+function cheatCodeActivated() {
+  alert("Cheat code activated!")
+}
+
+function resetKeyState() {
+  keyCount=0;
 }
